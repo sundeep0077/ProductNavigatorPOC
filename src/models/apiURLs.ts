f4e6a508f3c51cdc0f8b93ea231c656628bf4d6f
Sundeep Kunchala <sundeep0077@gmail.com>
2019-06-20 00:35:04 -0500

    Path.

--- src/models/apiURLs.ts
+++ src/models/apiURLs.ts
@@ -1,6 +1,6 @@
 export const API_URL = {
-    BaseURL :  'http://localhost:62758/api/',
-    //BaseURL :  'https://walmartpocapilayer.azurewebsites.net/api/',
+    //BaseURL :  'http://localhost:62758/api/',
+    BaseURL :  'https://walmartpocapilayer.azurewebsites.net/api/',
     GetAllCategories : 'Locator/GetAllCategories',
     FetchProductTraits : 'Locator/FetchProductTraits',
     FetchOptimizedRoute : 'Locator/FetchOptimizedRoute',
